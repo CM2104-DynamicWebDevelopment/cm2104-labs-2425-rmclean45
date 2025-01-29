ffaf7506cf488f0c6ef14daa8d01ad01e8fd0e90
rmclean <r.j.mclean2@rgu.ac.uk>
2025-01-29 17:49:10 +0000

    set public folder

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var knockknock = require('knock-knock-jokes');
 var app = express();
+
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
     res.send("Hello world! by express");
 });
