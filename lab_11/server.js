09de4de357f4d50dadbe916040ce7a645f862196
rmclean <r.j.mclean2@rgu.ac.uk>
2025-01-29 17:50:43 +0000

    missing semicolon

--- lab_11/server.js
+++ lab_11/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var knockknock = require('knock-knock-jokes');
 var app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.send("Hello world! by express");
