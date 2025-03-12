a9719f5b167138a35b52820b10708c04378d3c31
Ross McLean <r.j.mclean2@rgu.ac.uk>
2025-03-12 16:32:14 +0000

    emit message

--- lab_16/server.js
+++ lab_16/server.js
@@ -21,6 +21,10 @@ io.on('connection', function (socket) {
     socket.on('chat message', function (msg) {
       console.log('message: ' + msg);
   });
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
+});
+
 
 });
 
