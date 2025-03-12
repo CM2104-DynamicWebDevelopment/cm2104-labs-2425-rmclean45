913e02253e5d20ace69ad50be88ed9ca556c7cb9
Ross McLean <r.j.mclean2@rgu.ac.uk>
2025-03-12 16:22:23 +0000

    messages to server

--- lab_16/public/scripts/main.js
+++ lab_16/public/scripts/main.js
@@ -1,2 +1,10 @@
 var socket = io();
 
+$('#form').submit(function () {
+    var message = $('#input').val();
+    if (message) {
+        socket.emit('chat message', message);
+        $("#input").val("");
+    }
+    return false; 
+})
